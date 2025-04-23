41c8c989f56257bb2cf79b8b912b7580a3ea9f5f
Szabolcs Balogh <baloghszabolcs43@gmail.com>
2025-04-23 15:35:32 +0300

    Add core JavaScript logic and responsive media queries

---
+++ README.md
@@ -0,0 +1,11 @@
+# 🐱 Cat Photo Generator
+
+## 📖 About
+
+This is a small practice project built with HTML, CSS, and JavaScript.  
+When the user clicks a button, a new cat image is loaded from a Cat API.  
+Each click shows a different cute cat photo. 🐾
+
+The purpose of this project is to practice using APIs, DOM manipulation, and GitHub version control.
+
+Feel free to explore and have fun!
--- app.js
+++ app.js
@@ -1,4 +1,40 @@
-// Cat API
-// https://api.thecatapi.com/v1/images/search
+function imageElement(id, alt, src) {
+  const img = document.createElement('img');
+  Object.assign(img, {
+    id: `${id}`,
+    alt: `${alt}`
+  })
+  return img;
+}
 
+function buttonElement(text, id) {
+  const button = document.createElement('button');
+  button.textContent = `${text}`;
+  button.id = `${id}`;
+  return button;
+}
 
+async function fetchCats() {
+  let photoUrl;
+  try {
+    const response = await fetch('https://api.thecatapi.com/v1/images/search');
+    const parseData = await response.json();
+    photoUrl = await parseData[0].url;
+  } catch (error) {
+    console.error('Failed to load cat images:', error)
+  }
+  return photoUrl;
+}
+
+function main() {
+  const container = document.querySelector('.container');
+  const loadButton = buttonElement('Click Me', 'load-button');
+  let catPhoto = imageElement('image', 'A picture with a cat.');
+  loadButton.addEventListener('click', async () => {
+    let returnedData = await fetchCats();
+    catPhoto.src = returnedData;
+  });
+  container.append(catPhoto, loadButton);
+}
+
+window.addEventListener('DOMContentLoaded', main);
